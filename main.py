aafd3295e2093d1b40dad262749e83e9259f9c95
leo <leomerzlyakov@gmail.com>
2020-11-07 14:09:48 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+from dotenv import load_dotenv
+
+import telegram
+
+load_dotenv()
+
+
+CHAT_ID = os.getenv('CHAT_ID')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+tg_bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+
+def send_message(text_massge):
+    return tg_bot.send_message(chat_id=CHAT_ID, text=text_massge)
+
+
+def main():
+    mesege_number = 1
+    while True:
+        try:
+            text = f"Messege # {mesege_number}"
+            send_message(text)
+            time.sleep(5)
+            mesege_number += 1
+
+        except Exception as e:
+            print(f'Бот упал с ошибкой: {e}')
+            time.sleep(5)
+            continue
+
+
+if __name__ == '__main__':
+    main()
